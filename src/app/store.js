dd7fecb0f54d281991f03878479d00445b3e5906
yucj <nakts0123@gmail.com>
2020-11-09 23:34:43 +0800

    feat: add redux

---
+++ src/app/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+// `configureStore` API help us to
+// https://redux.js.org/recipes/configuring-your-store#simplifying-setup-with-redux-toolkit
+export default configureStore({
+  reducer: {},
+});
