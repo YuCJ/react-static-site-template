80cbb344050da6f96cc89679be69b4dce9d5c411
yucj <nakts0123@gmail.com>
2020-11-09 22:18:26 +0800

    chore: add React.StrictMode

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import App from './components/app'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
